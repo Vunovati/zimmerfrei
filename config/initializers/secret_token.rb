21de8e92fe97310a246accb1a97ee4e3cc8040a1
Vladimir Adamić <vladimir.adamic@gmail.com>
2011-03-02 12:47:31 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zimmerfrei::Application.config.secret_token = 'af7fcbb8cee0d2890491b4f2735282ddb3a60870c01252cbb84609aeeaa18ba158088edf2bf8763cf9025377ba79005d8e6a6fc14ffd911cc48841b5c5afbc3d'
